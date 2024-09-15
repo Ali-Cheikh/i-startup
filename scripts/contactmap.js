631650306053d08b65112e2fc350c017a90806a7
Ali <alouloucheikh05@gmail.com>
2024-09-15 21:14:21 +0100

    "Updated button text and added link to '404.html' in contactmap.js"

--- scripts/contactmap.js
+++ scripts/contactmap.js
@@ -6,11 +6,11 @@ async function getUserDetails() {
 
         let ip = data.ip;
         let time = data.time_zone.current_time.split(" ")[1].substring(0, 5);
-        document.getElementById('user-details').innerHTML = `If you are interested in Joining Our Team<br> Time now : ${time} <button id="join-waitlist" class="mt-6 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 font-manrope font-bold">Join the Waitlist</button><br>`;
+        document.getElementById('user-details').innerHTML = `If you are interested in Joining Our Team<br> Time now : ${time} <button id="join-waitlist" class="mt-6 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 font-manrope font-bold"><a href="404.html">Join the<br>Wait-list</a></button><br>`;
 
         setInterval(() => {
             let currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-            document.getElementById('user-details').innerHTML = `Join Our Team<br>Your IP: ${ip}<br> Time: ${currentTime} <button id="join-waitlist" class="mt-6 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 font-manrope font-bold">Join the Waitlist</button><br>`;
+            document.getElementById('user-details').innerHTML = `Join Our Team<br>Your IP: ${ip}<br> Time: ${currentTime} <button id="join-waitlist" class="mt-6 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 font-manrope font-bold"><a href="404.html">Join the<br>Wait-list</a></button><br>`;
         }, 30000); // Refresh time every 30 seconds
         initMap(34.7686605, 10.7492971);
 
