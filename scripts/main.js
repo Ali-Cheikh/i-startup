a8df7d3ee2019573d50a68a840e82d2c1b4a9743
Ali <alouloucheikh05@gmail.com>
2025-03-13 02:27:15 +0100

    Update programs.html layout and content for improved presentation and user engagement

--- scripts/main.js
+++ scripts/main.js
@@ -7,35 +7,3 @@ document.addEventListener('scroll', () => {
         }
     });
 });
-
-window.onload = function() {
-    // Check if the script is running
-    console.log("Script is running");
-
-    // Get the current URL path
-    const currentPath = window.location.pathname;
-    console.log("Current Path:", currentPath);
-
-    // Mapping paths to new paths
-    const pathMap = {
-        '/index.html': '/Home',
-        '/contact.html': '/Contact-Page',
-        '/services.html': '/Services-Page',
-        '/application.html': '/Application-Process',
-        '/programs.html': '/Programs-Page',
-        '/success-stories.html': '/Success-stories✨',
-        '/incubationapp.html': '/Incubation',
-        '/accelerationapp.html': '/Acceleration'
-    };
-
-    // Get the new path based on the current path
-    const newPath = pathMap[currentPath];
-    console.log("New Path:", newPath);
-
-    // If a new path exists, update the URL
-    if (newPath) {
-        const newUrl = window.location.protocol + "//" + window.location.host + newPath;
-        console.log("New URL:", newUrl);
-        window.history.pushState({}, "", newUrl);  // Change the URL without reloading the page
-    }
-};
